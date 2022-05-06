65e047a0818e997097bae6aef577dab05651a818
Harry Penlington <harry.penlington@gmail.com>
2022-05-06 16:01:27 +0100

    enable upload

--- util/exportList.js
+++ util/exportList.js
@@ -1,11 +1,11 @@
-// import { db } from 'db';
+import { db } from "db";
 
 const downloadJSON = (object, filename) => {
-  const blob = new Blob([JSON.stringify(object)], { type: 'application/json' });
+  const blob = new Blob([JSON.stringify(object)], { type: "application/json" });
   const url = window.URL.createObjectURL(blob);
-  const a = document.createElement('a');
-  a.setAttribute('href', url);
-  a.setAttribute('download', filename);
+  const a = document.createElement("a");
+  a.setAttribute("href", url);
+  a.setAttribute("download", filename);
   document.body.appendChild(a);
   a.click();
   a.parentElement.removeChild(a);
@@ -15,14 +15,14 @@ const exportList = async (listID) => {
   const list = await db.lists.get(listID);
   const exportGroup = async (groupID) =>
     db.buttons
-      .where('groupID')
+      .where("groupID")
       .equals(groupID)
       .toArray()
       .then((res) => {
         return res.map((e) => e.text);
       });
 
-  const groups = await db.groups.where('listID').equals(listID).toArray();
+  const groups = await db.groups.where("listID").equals(listID).toArray();
   const ngroups = await Promise.all(groups.map((e) => exportGroup(e.id)));
   const data = { name: list.name, groups: ngroups };
 
