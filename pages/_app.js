bbe59b726d433b6518e1f72a66cd785a29b071d1
Harry Penlington <harry.penlington@gmail.com>
2022-05-09 12:05:00 +0100

    Add favicon ixon sizes

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,28 @@ function MyApp({ Component, pageProps }) {
     <StrictMode>
       <Head>
         <title>Clipboard Helper</title>
-        <link rel="icon" href="/icon.svg" />
+
+        <link rel="icon" type="image/png" href="/icon-32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/icon-64.png" sizes="64x64" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/icon-128.png"
+          sizes="128x128"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/icon-256.png"
+          sizes="256x256"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/icon-512.png"
+          sizes="512x512"
+        />
+
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <Component {...pageProps} />
